e4153002ce9f0f7318a9b2fbc6df2124b391c799
ravenlee094 <ravenlee0327@gmail.com>
2018-07-19 14:42:21 -0400

    express, mongoose, port running

---
+++ server.js
@@ -0,0 +1,18 @@
+///// DEPENDENCIES /////
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+///// PORT /////
+const port = 3000;
+
+///// LISTENER /////
+app.listen(port, () => {
+  console.log('Listening on port: ', port);
+})
+
+///// CONNECTIONS /////
+mongoose.connect('mongodb://localhost:27017/cocktails', {useNewUrlParser: true});
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to mongoose...');
+});
