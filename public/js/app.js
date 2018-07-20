81a99a110b4de7d5c2a3471c17ff3d2e8d0afdde
ravenlee094 <ravenlee0327@gmail.com>
2018-07-20 16:41:51 -0400

    able to search for drinks & view in console

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,21 @@ app.controller('MainController', ['$http', function($http) {
   this.cocktailName = '';
   this.cocktails = [];
 
-  this.test = "Raven!";
+  this.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php/?'
+  this.apikey = 'apikey=' + '1'
+  this.query = 's='
+  this.searchURL = this.baseURL + this.apikey + '&' + this.query
+  console.log(this.searchURL);
 
-}]);
+  this.getCocktails = () => {
+    $http({
+      method: 'GET',
+      url: this.searchURL + this.cocktailName
+    }).then(response => {
+      console.log(response.data)
+    }, error => {
+      console.log(error)
+    }).catch(err => console.log('Catch: ', err))
+  }
+
+}]); //Closes app controller
