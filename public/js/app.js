6d5d726c9c01f137fa9cd08ed36ece9990483c6b
Sam <schung015@gmail.com>
2018-07-20 10:07:35 -0700

    cocktails.js model complete, index.html cocktail display complete

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ const app = angular.module('MyApp', []);
 app.controller('MainController', ['$http', function($http) {
   // controller var
   const controller = this;
+  // empty string for cocktail name
+  this.cocktailName = '';
 
   this.test = "Hello!";
 
