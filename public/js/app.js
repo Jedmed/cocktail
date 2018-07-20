9b2335f8844542674bf65163b18d41c81f845af3
Sam <schung015@gmail.com>
2018-07-20 10:14:33 -0700

    added cocktail name and empty cocktails array to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ const app = angular.module('MyApp', []);
 app.controller('MainController', ['$http', function($http) {
   // controller var
   const controller = this;
+  this.cocktailName = '';
+  this.cocktails = [];
 
   this.test = "Hello!";
 
