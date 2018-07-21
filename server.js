b847a56386a49568e0b2762672c30c72467af9aa
Sam <schung015@gmail.com>
2018-07-21 12:18:59 -0700

    set up sessions controller and required it in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/cocktails';
+const session = require('express-session');
 
 ///// MIDDLEWARE /////
 app.use(express.urlencoded({ extended: false }))
@@ -12,9 +13,22 @@ app.use(express.static('public'));
 ///// PORT /////
 const port = process.env.PORT || 3000;
 
+app.get('/app', (req, res)=>{
+    if(req.session.currentUser){
+        res.json(req.session.currentUser);
+    } else {
+        res.status(401).json({
+            status: 401,
+            message: 'not logged in'
+        });
+    }
+})
+
 // CONTROLLERS
 const cocktailsController = require('./controllers/cocktailController.js');
 app.use('/cocktails', cocktailsController);
+const sessionsController = require('./controllers/sessions.js');
+app.use('/sessions', sessionsController);
 
 
 ///// LISTENER /////
