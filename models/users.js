f5dd99637b631406dc5270ed9c256735cfe3095a
Joseph <josephedmed@hotmail.com>
2018-07-24 20:24:46 +0700

    changes user model for heroku

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  username: { type: String, unique: true},
+  username: String,
   password: String,
 });
 
